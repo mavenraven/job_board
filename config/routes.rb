9400c0a430bbd9ff7df01396724abcdde2f5c25a
mavenraven <developers@mavenraven.org>
2024-05-26 17:06:18 -0400

    homes controller cleaned up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :homes
+  root "homes#index"
   resources :jobs
   resources :companies
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
