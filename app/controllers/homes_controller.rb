62f95104fbcbe6694080fd2e8f511da529b3521e
mavenraven <developers@mavenraven.org>
2024-05-27 18:16:54 -0400

    cool, pagination with anchor

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   include Pagy::Backend
   def index
-    @pagy, @jobs = pagy(Job.all.order(created_at: :desc))
+    @pagy, @jobs = pagy(Job.all.order(created_at: :desc), fragment: '#jobs')
   end
 
   def show
