4b8fdb90e43db7b6b5be220b208c953ddf4e3aec
mavenraven <developers@mavenraven.org>
2024-05-26 21:52:02 -0400

    --wip-- [skip ci]

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-        title: ['Roboto', 'sans-serif'],
+        title: ['Paytone One', 'sans-serif'],
       },
     },
   },
